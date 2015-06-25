b7f2d8c505494eb7cac6743a092cc38bccec81b9
Larry Bradley <larry.bradley@gmail.com>
2015-06-25 19:49:58 -0400

    Add astropy affiliated package template

---
+++ imgeom/__init__.py
@@ -0,0 +1,17 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+"""
+Tools for dealing with mapping coordinates, measurements, and models
+between overlapping images.
+"""
+
+# Affiliated packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+from ._astropy_init import *
+# ----------------------------------------------------------------------------
+
+# For egg_info test builds to pass, put package imports here.
+if not _ASTROPY_SETUP_:
+    # from .core import *
+    pass
